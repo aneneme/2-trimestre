dcb8783ade9882d4b49f41df92b5fd46e6f467e3
aneneme <148064049+aneneme@users.noreply.github.com>
2024-05-28 10:13:48 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal=document.querySelector(".caixaPrincipal");
+const caixaPergutas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
 
+const perguntas=[
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas:[
+            "Alternativa 1",
+            "Alternativa 2",
+        ]
+    },
+];
